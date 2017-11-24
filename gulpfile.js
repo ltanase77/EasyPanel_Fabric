92f7294794bb2da761ea3231de4788a8220bfdac
Lucian Tanase <ltanase77@gmail.com>
2017-11-24 13:28:01 +0200

    Adding a gulp task that is minifying the javascript file and ignoring the minified files in git

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 var sassFiles = ['sass/reset.scss', 'sass/*.scss']
 
-gulp.task('serve', ['sass'], function() {
-	browserSync.init({
-		proxy: 'http://127.0.0.1:8080/WordApi/Fabric',
-		files: ['css/*.css', 'sass/*.scss']
-	});
+gulp.task('serve', ['sass', 'scripts'], function() {
+    browserSync.init({
+        proxy: 'http://127.0.0.1:8080/WordApi/Fabric',
+        files: ['css/*.css', 'sass/*.scss', 'js/*.js']
+    });
 
-	gulp.watch('sass/*.scss', ['sass']);
-	gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('sass/*.scss', ['sass']);
+    gulp.watch('js/*.js' ['scripts']);
+    gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 
 gulp.task('sass', function() {
-	return gulp.src(sassFiles)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('css'))
-		.pipe(browserSync.stream());
+    return gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('js/min'))
+        .pipe(browserSync.stream())
 });
 
 
